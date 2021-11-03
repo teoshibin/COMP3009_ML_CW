2fe0cfa478f1c40dc1b5e5b8186b9caea52f61ed
Teo Shi Bin <teoshibin@gmail.com>
2021-11-03 15:19:02 +0000

    half done inner validation loop

---
+++ cw1/functions/splitPartition.m
@@ -0,0 +1,28 @@
+function [test_data, train_data] = splitPartition(data, range_indices, random_perm, partition_selection)
+    %SPLITPARTITION split actual data using partitionIndex and select one
+    %single test partition
+    %
+    %   Data data before partitioning
+    %
+    %   Range_indices rows of (1,10) tuples contianing start and end of one
+    %   partition
+    %
+    %   random_perm a mapping for randomizing partition instances
+    % 
+    %   partition_selection selecting which partition to select
+    %   (1=>x=>k) where k is the number of partitions
+    %
+    
+    % single partition [1 2 3] = from range [1 : 3]
+    range_list = [range_indices(partition_selection, 1) : range_indices(partition_selection, 2)];
+    
+    % random number mapping e.g. random_perm = [3 9 2 5 8 7 6 1]
+    % single shuffled partition [3 9 2] = random_perm([1 2 3])
+    shuffled_test_indices = random_perm(range_list);
+    is_test_mat = false(height(data), 1);
+    is_test_mat(shuffled_test_indices) = true;
+
+    test_data = data(is_test_mat == 1, :);
+    train_data = data(is_test_mat == 0, :);
+end
+
