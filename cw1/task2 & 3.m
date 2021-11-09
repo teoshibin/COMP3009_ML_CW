f5075e115feb0c911c04c6f85fc6d37024133b81
khai99 <khaichuinhow@gmail.com>
2021-11-09 00:03:31 +0000

    update demo polynomial SVM regression

---
+++ cw1/task2 & 3.m
@@ -0,0 +1,24 @@
+%% all tuned models from nested cross validation scripts (6 scripts in total)
+
+%% Task 2 & 3
+% code for all the best tuned models containing the numbers of the support
+% vectors and the F1 score for classification and RMSE for regression are 
+% located at all the files below (.m files without prefix "demo" contain the
+% same codes as mlx files)
+
+%% Location of the code
+% F1 score for classification is located at the final_model section within
+% all mlx
+% RMSE for regression is located at the final_model section within all mlx
+% support vector count is at the end of the line within all mlx
+
+
+% demo_linearSVMClassification.mlx
+% demo_gaussianSVMClassification.mlx
+% demo_polynomialSVMClassification.mlx
+% demo_linearSVMRegression.mlx
+% demo_gaussianSVMRegression.mlx
+% demo_polynomialSVMRegression.mlx
+
+
+
