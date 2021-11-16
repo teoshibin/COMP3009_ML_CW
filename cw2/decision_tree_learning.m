c9814dc2e4a02ce0eadf0fa70b28e1497fdb42a3
qkloh25 <lohqiankai@gmail.com>
2021-11-16 12:45:31 +0000

    small progress

--- cw2/decision_tree_learning.m
+++ cw2/decision_tree_learning.m
@@ -1,9 +1,20 @@
 function [best_attribute, best_threshold] = Choose_Attribute(features, targets)
+    for attribute = 1:width(features)
+        attribute_column = features(:,attribute)
+        distinct_value = unique(attribute_column)
+        
+        for value = distinct_value
+            attribute_column
+        end
+        
+    end
     
 
 end
 
-function decision_tree = decision_tree_learning(features, labels)
+function decision_tree = decision_tree_learning(features, labels, task_type =1)
+    if task_type == 1:
+        
     decision_tree = decision_tree()
     if all(labels == labels(1))
         decision_tree.prediction = labels(1)
@@ -15,7 +26,7 @@ function decision_tree = decision_tree_learning(features, labels)
         
         decision_tree.kids(end+1) = decion_tree_learning(left_examples, left_targets);
         
-        right_examples = = features(features(:,best_attribute) >= best_threshold,:);
+        right_examples = features(features(:,best_attribute) >= best_threshold,:);
         right_targets = targets(features(:,best_attribute) >= best_threshold);
         
         decision_tree.kids(end+1) = decion_tree_learning(left_examples, left_targets);
