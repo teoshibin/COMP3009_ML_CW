e120d96a05dacad311431190c6cbdbb7a59312c5
How Khai Chuin <62005558+khai99@users.noreply.github.com>
2021-11-16 12:43:07 +0000

    Fix return

--- cw2/decision_tree_learning.m
+++ cw2/decision_tree_learning.m
@@ -7,7 +7,7 @@ function decision_tree = decision_tree_learning(features, labels)
     decision_tree = decision_tree()
     if all(labels == labels(1))
         decision_tree.prediction = labels(1)
-        return decision_tree
+    
     else
         [best_attribute, best_threshold] = Choose_Attribute(features, targets);
         left_examples = features(features(:,best_attribute) < best_threshold,:);
@@ -15,12 +15,12 @@ function decision_tree = decision_tree_learning(features, labels)
         
         decision_tree.kids(end+1) = decion_tree_learning(left_examples, left_targets);
         
-        right_examples = = features(features(:,best_attribute) >= best_threshold,:);
+        right_examples = features(features(:,best_attribute) >= best_threshold,:);
         right_targets = targets(features(:,best_attribute) >= best_threshold);
         
         decision_tree.kids(end+1) = decion_tree_learning(left_examples, left_targets);
         
-        return decision_tree
+       
         
     end
 
