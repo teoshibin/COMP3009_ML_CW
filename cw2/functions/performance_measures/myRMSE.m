4325d8f36ec8682bdba1d956b51b003a6de668c6
How Khai Chuin <62005558+khai99@users.noreply.github.com>
2021-11-19 22:28:19 +0000

    Update RMSE

--- cw2/functions/performance_measures/myRMSE.m
+++ cw2/functions/performance_measures/myRMSE.m
@@ -1,6 +1,5 @@
-function RMSE = myRMSE(model, test_X, test_Y)
-    
-    prediction = model.predict(test_X);
-    RMSE = sqrt(mean((prediction - test_Y).^2));
+function RMSE = myRMSE(actual_Y, predict_Y)
+
+    RMSE = sqrt(mean((predict_Y - actual_Y).^2));
 
 end
