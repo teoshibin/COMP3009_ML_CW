c385a300fdf16cbb315c87198eb47045b7573801
ShiBin Teo <62151367+teoshibin@users.noreply.github.com>
2021-12-01 16:37:50 +0000

    more fiddling with different loss functions

--- cw3/classification.py
+++ cw3/classification.py
@@ -16,13 +16,13 @@ logging.basicConfig(level=logging.ERROR)
 
 import matplotlib.pyplot as plt
 
-import os 
-# import pandas
+import os
+from custom_functions import *
+
 import time
 
 start = time.time()
 
-# %%
 tf.set_random_seed(69)
 
 #Network parameters
@@ -33,9 +33,11 @@ n_hidden3 = 6
 n_output = 2
 
 #Learning parameters
-learning_constant = 0.05
-number_epochs = 500
-batch_size = 1000
+learning_constant = 0.03
+max_epoch = 500
+
+
+# batch_size = 1000
 
 #Defining the input and the output
 X = tf.placeholder("float", [None, n_input])
@@ -65,101 +67,7 @@ def multilayer_perceptron(input_x):
     out_layer = one_layer_perceptron(layer_2, 6, 2, "softmax")
     return out_layer
 
-# def multilayer_perceptron(input_d):
-#     #DEFINING WEIGHTS AND BIASES
-#     b1 = tf.Variable(tf.random_normal([n_hidden1]))
-#     b2 = tf.Variable(tf.random_normal([n_hidden2]))
-#     b3 = tf.Variable(tf.random_normal([n_hidden3]))
-#     b4 = tf.Variable(tf.random_normal([n_output]))
-#     w1 = tf.Variable(tf.random_normal([n_input, n_hidden1]))
-#     w2 = tf.Variable(tf.random_normal([n_hidden1, n_hidden2]))
-#     w3 = tf.Variable(tf.random_normal([n_hidden2, n_hidden3]))
-#     w4 = tf.Variable(tf.random_normal([n_hidden3, n_output]))
 
-#     layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(input_d, w1), b1))
-#     layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, w2), b2))
-#     layer_3 = tf.nn.sigmoid(tf.add(tf.matmul(layer_2, w3), b3))
-#     out_layer = tf.nn.softmax(tf.add(tf.matmul(layer_3, w4),b4))
-#     return out_layer
-
-
-# %%
-def loadHeartFailureDataset():    
-
-    path = os.path.join("datasets","heart_failure_clinical_records_dataset.csv")
-    data = np.genfromtxt(path, delimiter=",", names=True)
-
-    data = data.view(np.float64).reshape((len(data), -1))
-    x_data = data[:, 0:-1]
-    y_data = data[:, -1]
-    y_data = y_data.astype('int32')
-    #no_data = y_data
-    y_data = np.identity(2)[y_data] # one hot encoding
-
-    # print(x_data)
-    # print(y_data)
-    return x_data, y_data
-
-def oneHotEncoding(y_data):
-    y_data = np.identity(2)[y_data]
-    return y_data
-
-def unOneHotEncoding(y_data, axis):
-    y_data = np.argmax(y_data, axis)
-    return y_data
-
-def minMaxNorm(data):
-    spacing = 5
-    pct_min = np.percentile(data, spacing, axis=0)
-    pct_max = np.percentile(data, 100 - spacing, axis=0)
-    norm_data = np.zeros(data.shape)
-    for i in range(len(data[0])):
-        norm_data[:, i] = (data[:,i] - pct_min[i]) / (pct_max[i] - pct_min[i])
-
-    norm_data[norm_data > 1] = 1
-    norm_data[norm_data < 0] = 0
-
-    return norm_data
-
-def splitLabel(data, number_of_labels):
-    number_of_columns = len(data[0])
-
-    x_data = data[:, 0:-number_of_labels]
-    y_data = data[:, number_of_columns - number_of_labels : number_of_columns]
-    return x_data, y_data
-
-def mergeLabel(x_data, y_data):
-    return np.column_stack((x_data, y_data))
-
-def validationSplit(data, percentage):
-    portion = round(data.shape[0]*percentage)
-    train = data[0:portion]
-    test = data[portion:data.shape[0]]
-    return train, test
-
-def shuffleRow(data):
-    np.random.seed(2)
-    np.random.shuffle(data)
-    return data
-
-def f1Score(predicted, actual):
-    TP = tf.count_nonzero(predicted * actual)
-    TN = tf.count_nonzero((predicted - 1) * (actual - 1))
-    FP = tf.count_nonzero(predicted * (actual - 1))
-    FN = tf.count_nonzero((predicted - 1) * actual)
-    precision = TP / (TP + FP)
-    recall = TP / (TP + FN)
-    f1 = 2 * precision * recall / (precision + recall)
-    return f1
-# %%
-#Create model
-neural_network = multilayer_perceptron(X)
-#Define loss and optimizer
-# loss_op = tf.reduce_mean(tf.math.squared_difference(neural_network,Y))
-# loss_op = tf.keras.losses.mean_squared_logarithmic_error(neural_network, Y)
-# loss_op = tf.keras.losses.categorical_crossentropy(Y, neural_network)
-# loss_op = tf.keras.losses.binary_crossentropy(Y, neural_network)
-loss_op = tf.keras.losses.mean_squared_logarithmic_error(Y, neural_network)
 
 """ 
     different loss require different lr and epoch
@@ -167,63 +75,124 @@ loss_op = tf.keras.losses.mean_squared_logarithmic_error(Y, neural_network)
     higher lr = more exploration and requre less epoch but hard to converge
 """
 
+#Create model
+neural_network = multilayer_perceptron(X)
+
+#Define loss and optimizer
+
+# all these testing was done using lr = 0.03 termination_tolerence = 15
+
+# Total Time Elapsed:  7.7628843784332275
+# best_epoch: 72 best_f1: 0.6531
+# loss_op = tf.reduce_mean(tf.math.squared_difference(neural_network,Y))
+
+# Total Time Elapsed:  4.167112350463867
+# best_epoch: 38 best_f1: 0.6316
 # loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=neural_network,labels=Y))
-optimizer = tf.train.GradientDescentOptimizer(learning_constant).minimize(loss_op)
+
+# Total Time Elapsed:  4.726463556289673
+# best_epoch: 43 best_f1: 0.6327
+# loss_op = tf.keras.losses.mean_squared_logarithmic_error(neural_network, Y)
+
+# Total Time Elapsed:  8.137772560119629
+# best_epoch: 73 best_f1: 0.6735
+# loss_op = tf.keras.losses.categorical_crossentropy(Y, neural_network)
+
+# Total Time Elapsed:  8.104426145553589
+# best_epoch: 73 best_f1: 0.6735
+loss_op = tf.keras.losses.binary_crossentropy(Y, neural_network)
+
+# Total Time Elapsed:  5.015074014663696
+# best_epoch: 46 best_f1: 0.6304
+# loss_op = tf.keras.losses.squared_hinge(Y, neural_network)
+
+# Total Time Elapsed:  8.144623279571533
+# best_epoch: 73 best_f1: 0.6735
+# loss_op = tf.keras.losses.kullback_leibler_divergence(Y, neural_network)
+
+# optimizer = tf.train.GradientDescentOptimizer(learning_constant).minimize(loss_op)
+optimizer = tf.train.AdamOptimizer(learning_constant).minimize(loss_op)
 
 #Initializing the variables
 init = tf.global_variables_initializer()
 
 # Load dataset
 x_data, y_data = loadHeartFailureDataset()
+
+# clean & split
 x_data = minMaxNorm(x_data)
+
 number_of_labels = len(y_data[0])
 
-# train_x = x_data
-# train_y = y_data
-
 data = mergeLabel(x_data, y_data)
+
+# # duplicate instances of label 1 one time to balance the dataset
+# deathEventInstances = data[data[:,len(data[0]) - 1] == 1, :]
+
+# data = np.append(data, deathEventInstances, axis=0)
+
 data = shuffleRow(data)
+# np.savetxt("foo.csv", data, delimiter=",")
+
 train_set, test_set = validationSplit(data, 0.5)
 train_x, train_y = splitLabel(train_set, number_of_labels)
 test_x, test_y = splitLabel(test_set, number_of_labels)
 
-# print("\nTrain X:\n", train_x)
-# print("\nTrain Y:\n", train_y)
-# print("\nTest X:\n", test_x)
-# print("\nTest Y:\n", test_y)
-
+# %%
 with tf.Session() as sess:
     sess.run(init)
 
+    # average_losses = np.zeros(int(max_epoch / 10))
 
+    # termination stuff
+    terminate_tolerence = -1
+    current_tolerence = terminate_tolerence
+    best_f1 = 0
+    best_model = neural_network
+    best_epoch = 0
 
+    errors =np.zeros(int(max_epoch / 1))
     #Training epoch
-    average_losses = np.zeros(int(number_epochs / 500))
-    for epoch in range(number_epochs):
+    for epoch in range(max_epoch):
         sess.run(optimizer, feed_dict={X: train_x, Y: train_y})
         
+
         #Display the epoch
-        if epoch % 2 == 0:
+        if epoch % 1 == 0:
+
             output = neural_network.eval({X: test_x})
             actual = unOneHotEncoding(test_y,1)
             predicted = tf.argmax(output,1)
-            f1 = f1Score(predicted,actual)
-            print("Epoch: %d F1: %.4f" %(epoch, tf.keras.backend.get_value(f1)))
-            # myloss = tf.keras.losses.categorical_crossentropy(test_y, output)
-            # print(tf.keras.backend.get_value(myloss))
+            f1 = round(tf.keras.backend.get_value(f1Score(predicted,actual)), 4)
+            errors[int(epoch / 1)] = f1
 
-        # if epoch % 500 == 0:
+            # termination criteria
+            if f1 > best_f1:
+                best_f1 = f1
+                best_model = neural_network
+                best_epoch = epoch
+                current_tolerence = terminate_tolerence
+            else:
+                if current_tolerence != 0:
+                    current_tolerence = current_tolerence - 1
+                else:
+                    break
+                
+            print("Epoch: %d F1: %f Tolerance: %d" % (epoch, f1, current_tolerence))
+
+        # if epoch % 10 == 0:
         #     # Test model
         #     pred = (neural_network) # Apply softmax to logits
         #     mse_loss_obj = tf.keras.losses.MSE(pred,Y)
         #     loss = mse_loss_obj.eval({X: train_x, Y: train_y})
-        #     average_losses[int(epoch / 500)] = np.mean(loss)
-    
+        #     average_losses[int(epoch / 10)] = np.mean(loss)
+
+
     # plot overfitting loss over epoch
-    # plt.plot(average_losses)
-    # plt.ylabel("MSE Loss")
-    # plt.xlabel("Epoch / 500")
-    # plt.show()
+    plt.plot(errors)
+    plt.ylabel("Bin Cross Entropy Loss")
+    plt.xlabel("Epoch / 1")
+    plt.show()
 
     #tf.keras.evaluate(pred,batch_x)
     # output = neural_network.eval({X: test_x})
@@ -239,7 +208,7 @@ with tf.Session() as sess:
     #     plt.xlabel('instances')
     # plt.show()
    
-    output = neural_network.eval({X: test_x})
+    output = best_model.eval({X: test_x})
     # estimated_class=tf.argmax(pred, 1)#+1e-50-1e-50
    
     correct_prediction1 = tf.equal(tf.argmax(output,1),unOneHotEncoding(test_y,1))
@@ -249,7 +218,7 @@ with tf.Session() as sess:
     predicted = tf.argmax(output,1)
     f1 = f1Score(predicted,actual)
  
-    
+
     # print(unOneHotEncoding(test_y,1))
     # print(tf.keras.backend.get_value(tf.argmax(output,1)))
     print(tf.keras.backend.get_value(f1))
@@ -258,3 +227,4 @@ with tf.Session() as sess:
     end = time.time()
     print("Total Time Elapsed: ", end - start)
 
+    print(f"best_epoch: {best_epoch} best_f1: {best_f1}")
